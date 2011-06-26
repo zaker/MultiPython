84c8407437dd0dcc1a4dffbc0589adbb5565d480
Sondre Naustdal <sondre.naustdal@gmail.com>
2011-06-26 13:43:26 +0200

    Added snake class

---
+++ src/Snake/Snake.py
@@ -0,0 +1,15 @@
+import pygame
+
+class Snake():
+	def __init__(self,pos=(0,0),color=(0,0,0)):
+		self.head = pos
+		self.color = color
+		
+	def move(self):
+		pass
+	
+	def plot(self):
+		pass
+	
+	def check(self):
+		pass
